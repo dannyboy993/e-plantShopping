e33784555cda3489c17a5901a1d4caafecdb8b94
dannyboy993 <danilobojanic993@gmail.com>
2024-08-01 03:56:27 -0400

    s.c

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -248,7 +248,7 @@ const handlePlantsClick = (e) => {
     setShowCart(false);
   };
 
-  const [addetToCart, setAddedToCart] = useState({});
+  const [addedToCart, setAddedToCart] = useState({});
 
   const handleAddToCart = (product) => {
     dispatchEvent(addItem(product));
@@ -279,21 +279,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-                {plantsArray.map((category, index) => (
-    <div key={index}>
-        <h1><div>{category.category}</div></h1>
-        <div className="product-list">
-            {category.plants.map((plant, plantIndex) => (
-            <div className="product-card" key={plantIndex}>
-                <img className="product-image" src={plant.image} alt={plant.name} />
-                <div className="product-title">{plant.name}</div>
-                <div><p>{plant.description}</p></div>
-                <div className='product-price'>{plant.cost}</div>
-                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+        {plantsArray.map((category, index) => (
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="product-list">
+                    {category.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                        <div><p>{plant.description}</p></div>
+                        <div className='product-price'>{plant.cost}</div>
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                    </div>
+                    ))}
+                </div>
             </div>
-            ))}
-        </div>
-    </div>
     ))}
 
         </div>
