cb719a64da73fc7f357b0299d301c9861a5794c5
dannyboy993 <danilobojanic993@gmail.com>
2024-08-01 04:47:43 -0400

    third

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    items.forEach((item ) => {
+        totalAmount += item.cost * item.quantity;
+    })
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
    
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item.quantity + 1));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity === 0) {
+        dispatch(removeItem());
+    } else {
+        dispatch(updateQuantity(item.quantity - 1));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    
   };
 
   return (
