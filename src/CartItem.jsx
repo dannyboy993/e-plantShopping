d9b20a67ea04adbdb1b545f399d914a67e9339c0
dannyboy993 <danilobojanic993@gmail.com>
2024-08-02 03:58:18 -0400

    fourth comm

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -66,7 +66,7 @@ const CartItem = ({ onContinueShopping }) => {
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'>{calculateTotalAmount}</div>
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
@@ -75,6 +75,8 @@ const CartItem = ({ onContinueShopping }) => {
     </div>
   );
 };
+
+export const {updateQuantity, removeItem} = CartItem.actions;
 
 export default CartItem;
 
